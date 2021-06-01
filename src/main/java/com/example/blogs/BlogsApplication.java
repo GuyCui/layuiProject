d2ed93049dafeed8c47730cbf3d599bcd18cf0fe
GuyCui <18024642@qq.com>
2021-06-01 21:25:54 +0800

    增加 layui 系统项目

---
+++ src/main/java/com/example/blogs/BlogsApplication.java
@@ -0,0 +1,16 @@
+package com.example.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author cuimingzhi
+ */
+@SpringBootApplication
+public class BlogsApplication {
+
+  public static void main(String[] args) {
+        SpringApplication.run(BlogsApplication.class, args);
+    }
+}
